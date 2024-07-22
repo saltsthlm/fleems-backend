0d3bd5f29ae7ec761b56e4aa092250666ae9ec00
adamhedin <adam.s.hedin@gmail.com>
2024-07-22 13:12:21 +0200

    first commit

---
+++ src/main/java/com/lans/fleems/FleemsApplication.java
@@ -0,0 +1,13 @@
+package com.lans.fleems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lans/fleems/FleemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lans.fleems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
