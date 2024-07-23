ecd8eb0a2dfc65fbbe09c9125ff421ef19ff9c0c
adamhedin <adam.s.hedin@gmail.com>
2024-07-23 09:13:11 +0200

    update task and driver fields
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/lans/fleems/driver/Driver.java
+++ src/main/java/com/lans/fleems/driver/Driver.java
@@ -5,10 +5,13 @@ import com.lans.fleems.task.TaskDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.util.UUID;
 
@@ -22,6 +25,24 @@ public class Driver {
     @Id
     @UuidGenerator
     private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String licenseNumber;
+
+    @NotNull
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    private String phoneNumber;
+
+    @NotNull(message="{email.required}")
+    @Pattern(regexp = "[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+            + "[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+            + "(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9]"
+            + "(?:[A-Za-z0-9-]*[A-Za-z0-9])?",
+            message = "{invalid.email}")
+    private String emailAdress;
 
     public Driver(CreateDriverDto createDriverDto) {
     }
--- src/main/java/com/lans/fleems/task/Task.java
+++ src/main/java/com/lans/fleems/task/Task.java
@@ -1,15 +1,15 @@
 package com.lans.fleems.task;
 
 
+import com.lans.fleems.client.Client;
 import com.lans.fleems.client.ClientDto;
 import com.lans.fleems.client.CreateClientDto;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -23,7 +23,37 @@ public class Task {
     @UuidGenerator
     private UUID id;
 
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    private String startDestination;
+
+    private String endDestination;
+
+    @CreatedDate
+    private Date dateCreated;
+
+    private Date dateFinished;
+
+    private String expectedDistance;
+
+    private List<Leg> legs;
+
+    private String product;
+
+    private int payload;
+
+    private int quantity;
+
     public Task(CreateTaskDto createTaskDto) {
+        this.client = createTaskDto.client();
+        this.startDestination = createTaskDto.startDestination();
+        this.endDestination = createTaskDto.endDestination();
+        this.expectedDistance = createTaskDto.expectedDistance();
+        this.product = createTaskDto.product();
+        this.payload = createTaskDto.payload();
+        this.quantity = createTaskDto.quantity();
     }
 
     public Task(TaskDto taskDto) {
