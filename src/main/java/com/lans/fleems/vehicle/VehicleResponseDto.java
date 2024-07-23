e257b5728e67beda4f4b700b3069951dcb8ac9e8
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-23 10:05:22 +0200

    fix vehicle response object

--- src/main/java/com/lans/fleems/vehicle/VehicleResponseDto.java
+++ src/main/java/com/lans/fleems/vehicle/VehicleResponseDto.java
@@ -5,8 +5,22 @@ import com.lans.fleems.task.TaskResponseDto;
 
 import java.util.UUID;
 
-public record VehicleResponseDto(UUID id) {
+public record VehicleResponseDto(UUID id,
+                                 String licenseNumber,
+                                 double payload,
+                                 double height,
+                                 double weight,
+                                 double length,
+                                 String model) {
     public static VehicleResponseDto fromVehicle(Vehicle vehicle) {
-        return new VehicleResponseDto(vehicle.getId());
+        return new VehicleResponseDto(
+                vehicle.getId(),
+                vehicle.getLicenseNumber(),
+                vehicle.getPayload(),
+                 vehicle.getHeight(),
+                vehicle.getWeight(),
+                 vehicle.getLength(),
+                 vehicle.getModel()
+                );
     }
 }
