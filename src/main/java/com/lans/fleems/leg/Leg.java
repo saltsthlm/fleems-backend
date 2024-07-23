4b1e2acd611382758ce0bc21fb0eae67d2a8b308
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-23 09:17:32 +0200

    added correct value annotations in leg class

--- src/main/java/com/lans/fleems/leg/Leg.java
+++ src/main/java/com/lans/fleems/leg/Leg.java
@@ -1,13 +1,9 @@
 package com.lans.fleems.leg;
 
 import com.lans.fleems.driver.Driver;
-import com.lans.fleems.task.CreateTaskDto;
 import com.lans.fleems.task.Task;
-import com.lans.fleems.task.TaskDto;
 import com.lans.fleems.vehicle.Vehicle;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,12 +21,22 @@ public class Leg {
     @Id
     @UuidGenerator
     private UUID id;
+    @ManyToOne
+    @JoinColumn(name ="driver_id")
     private Driver driver;
+    @ManyToOne
+    @JoinColumn(name ="vehicle_id")
     private Vehicle vehicle;
+    @Column(name = "start_time", nullable = false)
     private LocalDateTime startTime;
+    @Column(name = "end_time", nullable = false)
     private LocalDateTime endTime;
+    @Column(name = "start_location", nullable = false)
     private String startLocation;
+    @Column(name = "end_location", nullable = false)
     private String endLocation;
+    @ManyToOne
+    @JoinColumn(name ="task_id")
     private Task task;
 
     public Leg(CreateLegDto createLegDto) {
