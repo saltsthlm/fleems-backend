b024aad847cdfa57bfce0c878a6e6c691820dc16
adamhedin <adam.s.hedin@gmail.com>
2024-07-23 10:15:00 +0200

    fix Client dto
    Co-authored-by: Nelson Janusson <hejnelson@gmail.com>
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>

--- src/main/java/com/lans/fleems/client/ClientResponseDto.java
+++ src/main/java/com/lans/fleems/client/ClientResponseDto.java
@@ -1,9 +1,12 @@
 package com.lans.fleems.client;
 
+import com.lans.fleems.task.Task;
+
+import java.util.List;
 import java.util.UUID;
 
-public record ClientResponseDto(UUID id, String name) {
+public record ClientResponseDto(UUID id, String name, List<Task> tasks) {
     public static ClientResponseDto fromClient(Client client) {
-        return new ClientResponseDto(client.getId(), client.getName());
+        return new ClientResponseDto(client.getId(), client.getName(), client.getTasks());
     }
 }
