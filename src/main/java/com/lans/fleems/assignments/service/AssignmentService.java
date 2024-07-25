0b55bffa7dbfe4966b6510fd7e26e96d28ca27fc
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-25 14:19:26 +0200

    add endpoints to get tasks for driver and vehicle
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/main/java/com/lans/fleems/assignments/service/AssignmentService.java
+++ src/main/java/com/lans/fleems/assignments/service/AssignmentService.java
@@ -2,10 +2,12 @@ package com.lans.fleems.assignments.service;
 
 import com.lans.fleems.assignments.model.Assignment;
 import com.lans.fleems.assignments.repository.AssignmentRepository;
+import com.lans.fleems.task.model.Task;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -18,5 +20,19 @@ public class AssignmentService {
 
     public Assignment createAssignment(Assignment assignment) {
         return assignmentRepository.createAssignment(assignment);
+    }
+    public List<Task> getTasksForDriver(UUID driverId) {
+        return assignmentRepository.getAllAssignments()
+                .stream()
+                .filter(e->e.getDriver().getId()==driverId)
+                .map(Assignment::getTask)
+                .toList();
+    }
+    public List<Task> getTasksForVehicle(UUID vehicleId) {
+        return assignmentRepository.getAllAssignments()
+                .stream()
+                .filter(e->e.getVehicle().getId()==vehicleId)
+                .map(Assignment::getTask)
+                .toList();
     }
 }
