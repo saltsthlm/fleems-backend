a9d454ba8fd9ceedb9fe10014a595c72b4d8693c
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-25 16:49:25 +0200

    i
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/main/java/com/lans/fleems/configuration/SecurityConfig.java
+++ src/main/java/com/lans/fleems/configuration/SecurityConfig.java
@@ -16,16 +16,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> authorize
+    SecurityFilterChain defaultChain(HttpSecurity http)throws Exception{
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public").permitAll()
                         .anyRequest().authenticated()
                 )
-
-                .oauth2Client(withDefaults())
-                .oauth2Login(withDefaults());
-
-        return http.build();
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                .build();
     }
 
 }
