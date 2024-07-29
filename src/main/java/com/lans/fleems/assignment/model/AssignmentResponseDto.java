bf03e3396f5061f8e5ad3cfd79143bcd8d2f3d31
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-29 15:16:23 +0200

    fixed correct return type for assignment
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/main/java/com/lans/fleems/assignment/model/AssignmentResponseDto.java
+++ src/main/java/com/lans/fleems/assignment/model/AssignmentResponseDto.java
@@ -1,21 +1,24 @@
 package com.lans.fleems.assignment.model;
 
 import com.lans.fleems.driver.model.Driver;
+import com.lans.fleems.driver.model.DriverResponseDto;
 import com.lans.fleems.task.model.Task;
+import com.lans.fleems.task.model.TaskResponseDto;
 import com.lans.fleems.vehicle.model.Vehicle;
+import com.lans.fleems.vehicle.model.VehicleResponseDto;
 
 import java.util.UUID;
 
 public record AssignmentResponseDto(
         UUID id,
-        Task task,
-        Driver driver,
-        Vehicle vehicle
+        TaskResponseDto task,
+        DriverResponseDto driver,
+        VehicleResponseDto vehicle
 ) { public static AssignmentResponseDto fromAssignment(Assignment assignment) {
     return new AssignmentResponseDto(
             assignment.getId(),
-            assignment.getTask(),
-            assignment.getDriver(),
-            assignment.getVehicle()
+            TaskResponseDto.fromTask(assignment.getTask()),
+            DriverResponseDto.fromDriver(assignment.getDriver()),
+            VehicleResponseDto.fromVehicle(assignment.getVehicle())
     );
 } }
