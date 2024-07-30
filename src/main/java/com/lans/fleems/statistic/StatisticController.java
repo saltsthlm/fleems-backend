f415891f620461fea12af535792a11898d0b090b
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-07-30 14:54:33 +0200

    add statistics endpoints
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/main/java/com/lans/fleems/statistic/StatisticController.java
+++ src/main/java/com/lans/fleems/statistic/StatisticController.java
@@ -1,4 +1,43 @@
 package com.lans.fleems.statistic;
 
+import com.lans.fleems.assignment.model.AssignmentResponseDto;
+import com.lans.fleems.leg.model.LegResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("${api.base-path}${api.controllers.stats}")
+@AllArgsConstructor
 public class StatisticController {
+    private final StatisticService statisticService;
+
+    @GetMapping("/violations/speed")
+    public ResponseEntity<List<LegResponseDto>> getSpeedViolations() {
+        return ResponseEntity.ok(statisticService.getSpeedViolations()
+                .stream()
+                .map(LegResponseDto::fromLeg).toList());
+    }
+    @GetMapping("/violations/rest")
+    public ResponseEntity<List<LegResponseDto>> getRestViolations() {
+        return ResponseEntity.ok(statisticService.getRestViolations()
+                .stream()
+                .map(LegResponseDto::fromLeg).toList());
+    }
+    @GetMapping("/assigned")
+    public ResponseEntity<Double> getPercentOfVehiclesAssigned() {
+        return ResponseEntity.ok(statisticService.getPercentOfVehiclesAssigned());
+    }
+
+    @GetMapping("/completed")
+    public ResponseEntity<Double[]> getCompletedThisYear() {
+        return ResponseEntity.ok(statisticService.getCompletedThisYear());
+    }
 }
--- src/main/java/com/lans/fleems/statistic/StatisticService.java
+++ src/main/java/com/lans/fleems/statistic/StatisticService.java
@@ -1,5 +1,64 @@
 package com.lans.fleems.statistic;
 
+import com.lans.fleems.assignment.model.Assignment;
+import com.lans.fleems.assignment.repository.AssignmentRepository;
+import com.lans.fleems.assignment.service.AssignmentService;
+import com.lans.fleems.leg.model.Leg;
+import com.lans.fleems.leg.repository.LegRepository;
+import com.lans.fleems.task.model.StateEnum;
+import com.lans.fleems.vehicle.repository.VehicleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class StatisticService {
+    private final LegRepository legRepository;
+    private final VehicleRepository vehicleRepository;
+    private final AssignmentRepository assignmentRepository;
+    private final AssignmentService assignmentService;
+
+    public List<Leg> getSpeedViolations() {
+       return legRepository.getAllLegs().stream().filter(this::isSpeedViolation).toList();
+    }
+    public List<Leg> getRestViolations() {
+        return legRepository.getAllLegs().stream().filter(this::isRestViolation).toList();
+    }
+    private boolean isRestViolation(Leg leg){
+        if(leg.getEndTime()==null){
+            return false;
+        }
+        return 8<(leg.getStartTime().until(leg.getEndTime(), ChronoUnit.HOURS));
+    }
+    private boolean isSpeedViolation(Leg leg){
+        if(leg.getEndTime()==null){
+            return false;
+        }
+        return 90<(leg.getDistanceDriven()/
+                (leg.getStartTime().until(leg.getEndTime(), ChronoUnit.HOURS)));
+    }
+
+    public Double getPercentOfVehiclesAssigned() {
+        double assignedVehicles = assignmentService.getAllActiveAssignments().size();
+        double vehicles = vehicleRepository.getAllVehicles().size();
+        return assignedVehicles/vehicles;
+    }
+
+    public Double[] getCompletedThisYear() {
+        Double[] monthlyAssignments = new Double[12];
+        List<Assignment> Assignments = assignmentService.getAllAssignments()
+                .stream()
+                .filter(e->e.getTask().getState().equals(StateEnum.FINISHED))
+                .filter(e->e.getTask().getDateFinished().isAfter(LocalDateTime.now().minusYears(1)))
+                .toList();
+        Assignments
+                .forEach(e->
+                        monthlyAssignments[(int) e.getTask().getDateFinished().until(LocalDateTime.now(),ChronoUnit.YEARS)] ++
+                        );
+        return monthlyAssignments;
+    }
 }
