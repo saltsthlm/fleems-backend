9a18491c3c5d74567c80d936bf620748875c36c4
nelsonJanusson <nelson.janusson@appliedtechnology.se>
2024-08-01 16:02:05 +0200

    remove security
    Co-authored-by: Liam Frosteman Neogard <liamfrostemanneogard@gmail.com>
    Co-authored-by: Supipi Algama <nirmanialgama@gmail.com>
    Co-authored-by: Adam Hedin <Adam.s.hedin@gmail.com>

--- src/main/java/com/lans/fleems/configuration/SecurityConfig.java
+++ src/main/java/com/lans/fleems/configuration/SecurityConfig.java
@@ -4,16 +4,12 @@ package com.lans.fleems.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
-
+/*
     @Bean
     SecurityFilterChain defaultChain(HttpSecurity http)throws Exception{
         return http
@@ -25,6 +21,17 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                 .build();
     }
+      security:
+    oauth2:
+      resourceserver:
+        jwt:
+          issuer-uri: https://accounts.google.com
+
+          <dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-oauth2-resource-server</artifactId>
+		</dependency>
+ */
 
 
 
